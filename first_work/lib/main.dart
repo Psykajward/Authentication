569d4e57e707c34d445332cd772802064d2ccccd
Mahmoud Osman <105177762+Psykajward@users.noreply.github.com>
2024-01-01 18:20:58 +0200

    Made login and signup pages , added firebase dependeces

---
+++ first_work/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:first_work/Auth/Login_page.dart';
+import 'package:first_work/Auth/Sign_up_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        hintColor: Colors.blueAccent,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black87),
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen(),
+      },
+    );
+  }
+}
+
+
