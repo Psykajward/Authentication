5c93bdd628384b99548499c77d593891ec74df7a
Mahmoud Osman <105177762+Psykajward@users.noreply.github.com>
2024-01-07 14:06:29 +0200

    updated the whole project to solve the Auth proplem and sucsesfuly did

---
+++ first_work_1/lib/Auth/Registration_page.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String email;
+  late String password;
+  bool _isRegistering = false;
+  String _errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: Text('Registration'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.grey, Colors.white],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Create an Account',
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 30.0),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your email',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 15.0,
+                      ),
+                    ),
+                    onChanged: (value) {
+                      email = value;
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your password',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 15.0,
+                      ),
+                    ),
+                    onChanged: (value) {
+                      password = value;
+                    },
+                  ),
+                  SizedBox(height: 30.0),
+                  _isRegistering
+                      ? CircularProgressIndicator()
+                      : ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isRegistering = true;
+                        _errorMessage = ''; // Clear previous errors
+                      });
+                      try {
+                        final newUser =
+                        await _auth.createUserWithEmailAndPassword(
+                          email: email,
+                          password: password,
+                        );
+                        if (newUser != null) {
+                          // Handle successful registration
+                          // Navigate to a new screen or perform any necessary action
+                        }
+                      } catch (e) {
+                        print(e);
+                        setState(() {
+                          _errorMessage =
+                          'Registration failed. Please try again.'; // Set error message
+                        });
+                      } finally {
+                        setState(() {
+                          _isRegistering = false;
+                        });
+                      }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.0),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.lightBlue,
+                      padding: EdgeInsets.symmetric(vertical: 15.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      elevation: 5.0,
+                      shadowColor: Colors.blueAccent,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  if (_errorMessage.isNotEmpty)
+                    Text(
+                      _errorMessage,
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 16.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  SizedBox(height: 20.0),
+                  TextButton(
+                    onPressed: () {
+                      // Implement your logic for additional action (e.g., go back, navigate somewhere else)
+                    },
+                    child: Text(
+                      'Already have an account? Log in',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ first_work_1/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:first_work_1/Auth/Login_page.dart';
+import 'package:flutter/material.dart';
+
+import 'Auth/Home_page.dart';
+import 'Auth/Registration_page.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        hintColor: Colors.blueAccent,
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black87),
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen(),
+        '/Home' : (context) => Home(),
+      },
+    );
+  }
+}
+
+
